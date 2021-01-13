d9652394ed94a95dbecb49aa7f32be2a3e7120dc
KengoKashihara <KengoKashihara1998@gmail.com>
2021-01-14 00:09:54 +0900

    シンプルなシーン遷移を実装

---
+++ EscapeGame/Assets/Scripts/SceneController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public enum SceneType
+{
+    TitleScene,     // タイトルシーン 
+    GameScene,      // ゲームシーン
+    ResultScene,    // リザルトシーン
+}
+
+public class SceneController : MonoBehaviour
+{
+    /// <summary>
+    /// シーン遷移
+    /// </summary>
+    /// <param name="scene">遷移するシーン</param>
+    public static void ChangeScene(SceneType scene)
+    {
+        string name = scene.ToString();
+        SceneManager.LoadScene(name);
+    }
+}
